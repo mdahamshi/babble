92b5ef4932793b7bae0c072cd3705a7f4f0b27db
mdahamshi <mmd.0@hotmail.com>
2017-08-27 11:25:28 +0300

    added loading overlay

--- client/scripts/main.js
+++ client/scripts/main.js
@@ -1,20 +1,55 @@
 var Babble = {
     $: document.querySelector.bind(document) ,
     $$: document.querySelectorAll.bind(document) ,
-    fixMargin: function(){
-        // document.querySelector('.bab-mainHeader img ').style['margin-top'] = document.getElementsByClassName('bab-mainHeader')[0].clientHeight * 0.15 + 'px';
-        // document.querySelector('main > header > h1').style['margin-top'] = document.querySelector('main > header').clientHeight * 0.372881356 + 'px';
+    // **************************** Requiered API*****************************************
+    register: function register(userInfo){
 
     },
+    getMessages: function getMessages(counter, callback){
+
+    },
+    postMessage: function postMessage(message, callback){
+
+    },
+    deleteMessage: function deleteMessage(id, callback){
+
+    },
+    getStats: function getStats(callback){
+
+    },
+    
     init: function(){
+
+        if (Storage && localStorage.getItem('babble') === null){
+            var modal = Babble.$('#bab-modal'),
+            overlay = Babble.$('#bab-modal-overlay');
+            modal.classList.remove('bab-u-hidden');
+            this.hideLoading();
+        }
+        else {
+            Babble.localStorage = localStorage.getItem('babble');
+        }
+
         this.makeGrowable(document.querySelector('.js-growable'));
+    },
+    delay: function delay(callback, time){
+        setTimeout(callback.bind(Babble), time);
+    },
+    hideLoading: function hideLoading(){
+        Babble.$('#bab-loading')
+        .classList.add('bab-u-hidden');
     },
     makeGrowable: function makeGrowable(container) {
         var area = container.querySelector('textarea');
         var clone = container.querySelector('span');
         area.addEventListener('input', function(e) {
             clone.textContent = area.value;
+            Babble.scrollMessageSection();
         });
+    },
+    scrollMessageSection: function scrollMessageSection(){
+        var messageSection = Babble.$('#bab-messagesSection');
+        messageSection.scrollTop = messageSection.scrollHeight;
     },
     handleAnonymos: function handleAnonymos(){
         document.getElementById('bab-modal-overlay')
@@ -22,14 +57,16 @@ var Babble = {
         document.getElementById('bab-modal')
         .classList.add('bab-u-hidden');
     }
+    
 };
 
 
 window.onload = function(){
     
-    Babble.init();
+    Babble.delay(Babble.init, 1000);
 }
 window.onresize = function(){
+    Babble.scrollMessageSection();
 }
 
 document.onclick = function(evt){
