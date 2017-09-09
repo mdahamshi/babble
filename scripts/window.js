d7ab0ba1cbc5681e2493f2d82c287de288e51131
mdahamshi <mmd.0@hotmail.com>
2017-09-09 18:08:19 +0300

    added client to root, so speed test can pass

---
+++ scripts/window.js
@@ -0,0 +1,25 @@
+window.onload = function(){
+    console.log('window load')
+    if(localStorage.getItem('babble') == null)
+        localStorage.setItem('babble',JSON.stringify({userInfo:{email:"", name:""}, currentMessage:""}));
+    window.Babble.init();
+
+};
+window.onblur = function(){
+
+    window.Babble.updateLocalStorage();
+    
+
+};
+window.onresize = function(){
+    window.Babble.scrollMessageSection();
+};
+
+
+
+window.onbeforeunload = function(){
+    
+    window.Babble.updateLocalStorage();
+    window.Babble.logOut();
+    // return msg;
+};
